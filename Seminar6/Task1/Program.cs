f7b57893817eeccfa3e01dee995a393bcff3b115
SnezhannaPristavka <gorlovasv41@mail.ru>
2022-04-04 00:11:11 +0300

    Done Seminar6

---
+++ Seminar6/Task1/Program.cs
@@ -0,0 +1,38 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// Вариант1 не смогла довести до ума, как выйти из цикла. Не придумала ничего лучшего, чем выйти через число 0
+//но понимаю, что это не корректно.
+/*void InputNumber(int counter)
+{
+    try
+    {
+        Console.WriteLine("Введите число: ");
+        double num = Convert.ToDouble(Console.ReadLine());
+        while (num != 0)
+        {
+            if(num > 0) counter++;
+            Console.WriteLine("Введите число: ");
+            num = Convert.ToDouble(Console.ReadLine());
+        }
+        Console.WriteLine("Положительных чисел " + counter);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine("Ввод некорректен: " + e.Message);
+        InputNumber(counter);
+    }
+}
+int count = 0;
+InputNumber(count);*/
+
+//Вариант2 рабочий
+
+string x = "0";
+int count = 0;
+while(x.ToLower()!="stop")// подсвечивает, не знаю почему. Если убрать ToLower() светить не будет.
+{
+    double y = Convert.ToDouble(x);
+    if(y > 0) count++;
+    Console.WriteLine("Введите число: ");
+    x = Console.ReadLine(); // подсвечивает, не знаю почему.). На самой первой лекции в примере у лектора не светилось
+}
+Console.WriteLine("Положительных чисел " + count);
---
+++ Seminar6/Task2/Program.cs
@@ -0,0 +1,35 @@
+﻿//Пользователь вводит с клавиатуры кол-во строк и столбцов для массива. 
+//Вывести массив в виде матрицы, создав "рамку" из единиц, а внутреннюю зону матрицы заполнить нулями.
+// 3, 4 ->
+// 1 1 1 1
+// 1 0 0 1
+// 1 1 1 1
+void PrintArray (int [,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        matrix [i, 0] = 1;
+        matrix [i, matrix.GetLength(1)-1] = 1;
+    }
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix [0, j] = 1;
+        matrix [matrix.GetLength(0)-1, j] = 1;
+    }
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write ($"{matrix [i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write ("Введите количество строк в массиве ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите количество столбцов в массиве ");
+int column = Convert.ToInt32(Console.ReadLine());
+
+int [,] matrix = new int [row, column];
+PrintArray (matrix);
