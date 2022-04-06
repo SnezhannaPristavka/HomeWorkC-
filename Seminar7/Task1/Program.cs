3cc411c27551cf4e55b23aaa865c63115d97b8b8
SnezhannaPristavka <gorlovasv41@mail.ru>
2022-04-06 19:26:47 +0300

    Done Seminar7 Task1

---
+++ Seminar7/Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+void FillArray(int m, int n)
+{
+    double[,] matrix = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            Random rand = new Random();
+            matrix[i, j] = rand.Next(-100,101) * rand.NextDouble(); //0.0 до 1.0
+            Console.Write($"{Math.Round(matrix[i, j], 2)} ");
+        }
+    Console.WriteLine();
+    }
+}
+int row = 3;
+int column = 4;
+
+FillArray(row, column);
