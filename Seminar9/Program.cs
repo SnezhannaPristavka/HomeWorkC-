48f7736ca8e87d96f3cd29c36e81c8bb6138a381
SnezhannaPristavka <gorlovasv41@mail.ru>
2022-04-13 21:14:37 +0300

    Done Seminar9

---
+++ Seminar9/Program.cs
@@ -0,0 +1,24 @@
+﻿// Доработать и выслать релиазацию программы по выводу треугольника Паскаля.
+void ShowTriangle(int number)
+{
+    for (int i = 0; i < number; i++)
+    {
+        for (int j = number; j > i + 1; j--)
+        {
+            Console.Write(" ");
+        }
+
+        int val = 1;
+
+        for (int j = 0; j <= i; j++)
+        {
+            Console.Write(val + " ");
+            val = (val * (i - j)) / (j + 1);
+        }
+
+        Console.WriteLine();
+    }
+}
+Console.WriteLine("Введите порядок степени для треугольника");
+int num = Convert.ToInt32(Console.ReadLine());
+ShowTriangle(num);
